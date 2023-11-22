1c917a5b4b5bafcde866e30ab0214c389fb26839
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-11-22 20:05:49 +0100

    Add debug to app

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,14 +1,16 @@
 import "dotenv/config";
 import chalk from "chalk";
 import express from "express";
+import debugCreator from "debug";
 
+const debug = debugCreator("robots:root:server:app");
 const app = express();
 
 app.disable("x-powered-by");
 
 export const startServer = (port: number) => {
   app.listen(port, () => {
-    console.log(chalk.green(`Listening on http://localhost:${port}`));
+    debug(chalk.green(`Listening on http://localhost:${port}`));
   });
 };
 
